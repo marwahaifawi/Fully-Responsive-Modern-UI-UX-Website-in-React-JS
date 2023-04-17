a0169dad74333143c012d1e6678272923669bcd0
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-04-17 11:58:01 +0300

    Add the CTA  Section Design in Home Page

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,17 @@ import './CTA.css';
 
 const cta = () => {
   return (
-    <div>cta</div>
+    <div className='gpt3Cta'>
+      <div className="gpt3CtaContent">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3CtaButton">
+        <button type='button'>
+          Get Started
+        </button>
+      </div>
+    </div>
   )
 }
 
